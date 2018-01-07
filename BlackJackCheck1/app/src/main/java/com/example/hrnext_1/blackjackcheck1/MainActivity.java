4ba5a08001d0061d59ceed97479d8b561f58e95b
Shinji Fujita <s.fujita@hr-next.co.jp>
2018-01-07 20:49:08 +0900

    BlackJackApp 1/7

--- BlackJackCheck1/app/src/main/java/com/example/hrnext_1/blackjackcheck1/MainActivity.java
+++ BlackJackCheck1/app/src/main/java/com/example/hrnext_1/blackjackcheck1/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.hrnext_1.blackjackcheck1;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    String mWord1;
-    String mWord2;
-    int mNum1 = 0;
-    int mNum2 = 0;
-    int mSumNum = 0;
+    String mWordDecision;
+    String mWordCommand;
+    int mNumTotal = 0;
+    int mNumAdd = 0;
+    int mNumSum = 0;
     int mInputCount = 1;
 
     @Override
@@ -20,66 +22,89 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView1 = (TextView) findViewById(R.id.textView1);
-        final TextView textView2 = (TextView) findViewById(R.id.textView2);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
+        //初期処理
+        final ImageView imageViewDroid = (ImageView) findViewById(R.id.imageViewDroid);
+        final TextView textViewDecision = (TextView) findViewById(R.id.textViewDecision);
+        final TextView textViewCommand = (TextView) findViewById(R.id.textViewCommand);
         final EditText editText = (EditText) findViewById(R.id.editText);
-        mWord2 = getString(R.string.input_num1) + mInputCount + getString(R.string.input_num2);
-        textView2.setText(mWord2);
+        Button buttonExe = (Button) findViewById(R.id.buttonExe);
+        Button buttonClear = (Button) findViewById(R.id.buttonClear);
+        final int colorGold = getResources().getColor(R.color.colorGold);
+        final int colorRed = getResources().getColor(R.color.colorRed);
+        final int colorBlue = getResources().getColor(R.color.colorBlue);
+        final int colorDeepPink = getResources().getColor(R.color.colorDeepPink);
+        mWordCommand = getString(R.string.commandNumber) + mInputCount + getString(R.string.commandCount);
+        textViewCommand.setText(mWordCommand);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        //実行ボタン
+        buttonExe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //数値のみの入力制限を実装する
-//                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                //入力値の取得
                 String str = editText.getText().toString();
                 editText.setText("");
                 if (!(str == null || str.length() == 0)) {
                     ++mInputCount;
-                    if (mNum1 == 0) {
-                        mNum1 = Integer.parseInt(str);
-                        mWord2 = getString(R.string.input_num1) + mInputCount + getString(R.string.input_num2);
+                    //１回目の処理
+                    if (mNumTotal == 0) {
+                        mNumTotal = Integer.parseInt(str);
+                        mWordCommand = getString(R.string.commandNumber) + mInputCount + getString(R.string.commandCount) + getString(R.string.commandNow) + mNumTotal;
                     } else {
-                        mNum2 = Integer.parseInt(str);
-                        mSumNum = mNum1 + mNum2;
-                        mNum1 = mSumNum;
-                        if (mSumNum < 16) {
-                            mWord1 = getString(R.string.input_num3);
-                            mWord2 = getString(R.string.input_num1) + mInputCount + getString(R.string.input_num2);
-                        } else if (mSumNum > 21) {
-                            mWord1 = getString(R.string.input_num4);
-                            mWord2 = "";
+                        //２回目以降の処理
+                        mNumAdd = Integer.parseInt(str);
+                        mNumSum = mNumTotal + mNumAdd;
+                        mNumTotal = mNumSum;
+                        //HIT時の処理
+                        if (mNumSum < 16) {
+                            mWordDecision = getString(R.string.decision1);
+                            mWordCommand = getString(R.string.commandNumber) + mInputCount + getString(R.string.commandCount) + getString(R.string.commandNow) + mNumSum;
+                            textViewDecision.setTextColor(colorDeepPink);
+                            imageViewDroid.setImageResource(R.drawable.androidonskateboardwithonearmup);
+                        //PIG時の処理
+                        } else if (mNumSum > 21) {
+                            mWordDecision = getString(R.string.decision4);
+                            mWordCommand = "\n";
                             editText.setVisibility(View.INVISIBLE);
-                        } else if (mSumNum == 21) {
-                            mWord1 = getString(R.string.input_num5);
-                            mWord2 = "";
+                            textViewDecision.setTextColor(colorRed);
+                            imageViewDroid.setImageResource(R.drawable.androidwitharmuponskateboard);
+                        //BlackJack時の処理
+                        } else if (mNumSum == 21) {
+                            mWordDecision = getString(R.string.decision3);
+                            mWordCommand = "\n";
                             editText.setVisibility(View.INVISIBLE);
-                        } else if (mSumNum >= 16) {
-                            mWord1 = getString(R.string.input_num6);
-                            mWord2 = "";
+                            textViewDecision.setTextColor(colorGold);
+                            imageViewDroid.setImageResource(R.drawable.androidonskateboardwithtwoarmsup);
+                        //STAND時の処理
+                        } else if (mNumSum >= 16) {
+                            mWordDecision = getString(R.string.decision2);
+                            mWordCommand = "\n";
                             editText.setVisibility(View.INVISIBLE);
+                            textViewDecision.setTextColor(colorBlue);
+                            imageViewDroid.setImageResource(R.drawable.androidonskateboardwithleftarmraised);
                         }
                     }
-                    textView1.setText(mWord1);
-                    textView2.setText(mWord2);
+                    //判定結果の出力
+                    textViewDecision.setText(mWordDecision);
+                    textViewCommand.setText(mWordCommand);
                 }
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        //クリアボタン
+        buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mNum1 = 0;
-                mNum2 = 0;
-                mSumNum = 0;
+                mNumTotal = 0;
+                mNumAdd = 0;
+                mNumSum = 0;
                 mInputCount = 1;
-                mWord1 = "";
-                mWord2 = getString(R.string.input_num1) + mInputCount + getString(R.string.input_num2);
-                textView1.setText(mWord1);
-                textView2.setText(mWord2);
+                mWordDecision = "";
+                mWordCommand = getString(R.string.commandNumber) + mInputCount + getString(R.string.commandCount);
+                textViewDecision.setText(mWordDecision);
+                textViewCommand.setText(mWordCommand);
                 editText.setVisibility(View.VISIBLE);
                 editText.setText("");
+                imageViewDroid.setImageResource(R.drawable.androidonskateboard);
             }
         });
     }
